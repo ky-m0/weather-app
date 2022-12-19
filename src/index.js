b082c2b3db8b42e1dcb1584c81ef5e8833eca274
ky-m0 <114282955+ky-m0@users.noreply.github.com>
2022-12-19 21:00:01 +1100

    current location button functional

--- src/index.js
+++ src/index.js
@@ -46,16 +46,41 @@ let mins = ("0" + now.getMinutes()).slice(-2);
 let currentTime = document.querySelector(".currentTime");
 currentTime.innerHTML = `${hrs}:${mins}`;
 
-// 2. add search engine, update city name on page after submit
+// API response: update city temp in celsius (from either search OR current location)
+//let city = response.data.name;
 
-function searchCity(event) {
+function showTemp(response) {
+  //console.log(response.data.name);
+  console.log(response.data.main.temp);
+  tempCround = Math.round(response.data.main.temp);
+  console.log(tempCround);
+  let tempC = document.querySelector(".currentTemp");
+  tempC.innerHTML = tempCround;
+}
+
+// API call (using search)
+
+function apiCallSearch(event) {
+  let apiKey = "46fac47dd8b8fa26d1b6852218ad3dfe";
+  let units = "metric";
+  let h1City = document.querySelector("h1");
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${h1City.innerHTML}`;
+  axios.get(`${apiUrl}&appid=${apiKey}&units=${units}`).then(showTemp);
+}
+
+// Search form + update city name in h1 + trigger API call
+
+function searchForCity(event) {
   event.preventDefault();
   let inputCity = document.querySelector("#searchCity");
-  let currentCity = document.querySelector(".currentCity");
-  currentCity.innerHTML = inputCity.value;
+  console.log(inputCity.value);
+  let currentCity = document.querySelector("h1");
+  currentCity.innerHTML = `${inputCity.value}`;
+  return apiCallSearch();
 }
+// search form submit event listener
 let searchForm = document.querySelector("form");
-searchForm.addEventListener("submit", searchCity);
+searchForm.addEventListener("submit", searchForCity);
 
 // 3. Display a fake temperature (i.e 17) in Celsius and add
 //a link to convert it to Fahrenheit. When clicking on it,
@@ -63,12 +88,47 @@ searchForm.addEventListener("submit", searchCity);
 //When clicking on Celsius, it should convert it back to
 //Celsius.
 
-function currentTempF() {
-  let tempF = document.querySelector(".currentTemp");
-  tempF.innerHTML = "60";
+//function currentTempF() {
+// let tempF = document.querySelector(".currentTemp");
+// tempF.innerHTML = "60";
+// }
+
+// function currentTempC() {
+// let tempC = document.querySelector(".currentTemp");
+// tempC.innerHTML = "17";
+// }
+
+// current location API call
+
+// current location : update h1
+function showPosition(position) {
+  //console.log(position);
+  //console.log(position.coords.latitude);
+  //console.log(position.coords.longitude);
+
+  let apiKey2 = "46fac47dd8b8fa26d1b6852218ad3dfe";
+  let units2 = "metric";
+  let apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?`;
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  console.log(lat);
+  console.log(lon);
+  axios
+    .get(`${apiUrl2}lat=${lat}&lon=${lon}&appid=${apiKey2}&units=${units2}`)
+    .then(showTemp);
+
+  let h1City = document.querySelector("h1");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `Current location`;
 }
 
-function currentTempC() {
-  let tempC = document.querySelector(".currentTemp");
-  tempC.innerHTML = "17";
+// get current position
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
 }
+
+// current position event listener
+
+let button = document.querySelector("button");
+button.addEventListener("click", getCurrentPosition);
